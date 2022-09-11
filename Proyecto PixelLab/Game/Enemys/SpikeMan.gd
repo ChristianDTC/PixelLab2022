e0fc91213bfcd2b3dc9c29e05ce57409efdf5a91
chzepech@gmail.com <chzepech@gmail.com>
2022-09-11 10:31:44 -0300

    segunda instancia pixellab, hasta ahora cree el personaje, plataformas, objetos de daño y un enemigo

---
+++ Proyecto PixelLab/Game/Enemys/SpikeMan.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+export var velocity = 100.0
+
+var gravity = 100.0
+var move = Vector2.ZERO
+
+onready var animation = $AnimatedSprite
+onready var detect_empty = $DetectEmpty
+onready var detect_object = $DetectObject
+
+func _physics_process(delta):
+	fall()
+	run()
+	move_and_slide(move, Vector2.UP)
+
+func fall():
+	if not is_on_floor():
+		move.y = gravity
+
+func run():
+	if not animation.is_playing():
+		animation.play("run")
+		
+	detect_collision()
+		
+	move.x = velocity
+	
+func detect_collision():
+	if not detect_empty.is_colliding() or detect_object.is_colliding():
+		velocity *= -1
+		detect_empty.position *= -1
+		detect_object.position *= -1
+		detect_object.scale *= -1
+		anime(animation.flip_h)
+		
+
+func anime(value_present):
+	animation.flip_h = !value_present
+	
+
+func _on_DetecPlayer_body_entered(body):
+	body.respawn()
---
+++ Proyecto PixelLab/Game/Player/PlayerSaltarina.gd
@@ -0,0 +1,62 @@
+extends KinematicBody2D
+
+export var speed = Vector2(150.0, 150.0)
+export var acel_down = 400
+export var str_jump = 3000
+export var str_bounce = 350
+
+var move = Vector2.ZERO
+
+onready var animation = $AnimatedSprite
+onready var sfx_jump = $sfx_jump
+onready var camera = $Camera2D
+
+func _physics_process(delta):
+	move.x = speed.x * select_direction()
+	jump()
+	down()
+	move_and_slide(move, Vector2.UP)
+	crash_roof()
+	fall_down()
+
+func select_direction():
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	if direction == 0:
+		animation.play("idle")
+	else:
+		if direction < 0:
+			animation.flip_h = true
+		else:
+			animation.flip_h = false
+		animation.play("run")
+	
+	return direction
+
+func down():
+	if not is_on_floor():
+		animation.play("jump")
+		move.y += acel_down
+		move.y = clamp(move.y, -str_jump, speed.y)
+		
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		sfx_jump.play()
+		animation.play("jump")
+		move.y = 0
+		move.y -= str_jump
+
+func crash_roof():
+	if is_on_ceiling():
+		move.y = str_bounce
+	
+func fall_down():
+	if position.y > camera.limit_bottom:
+		respawn()
+		
+
+func respawn():
+	get_tree().reload_current_scene()
+		
+
+func _on_level1_ready():
+	pass # Replace with function body.
