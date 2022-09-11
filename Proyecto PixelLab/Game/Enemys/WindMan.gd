4d4be1c97bf513618dac3faf872c79ca57928658
chzepech@gmail.com <chzepech@gmail.com>
2022-09-11 20:58:20 -0300

    enemigo volador windman, ya tiene la animación de volar, activo el daño y rebote del player contra el

--- Proyecto PixelLab/Game/Enemys/WindMan.gd
+++ Proyecto PixelLab/Game/Enemys/WindMan.gd
@@ -1,4 +1,16 @@
 extends Area2D
 
+onready var detect_player = $DetectPlayer
+onready var detect_trample = $DetectTrample/Collision
+
 func _on_DetectTrample_body_entered(body):
+	disable_colliders([detect_player, detect_trample])
 	body.impulse()
+
+func _on_body_entered(body):
+	body.respawn()
+
+func disable_colliders(colliders):
+	for collision in colliders:
+		collision.set_deferred("disabled", true)
+		collision.set_deferred("visible", false)
