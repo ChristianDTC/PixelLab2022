bfac970a061f36fcc76f0c87694b8980c453af2c
chzepech@gmail.com <chzepech@gmail.com>
2022-09-11 21:09:56 -0300

    enemigo windman ya tiene la animación de morir y desaparece cuando lo hace

--- Proyecto PixelLab/Game/Enemys/WindMan.gd
+++ Proyecto PixelLab/Game/Enemys/WindMan.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 onready var detect_player = $DetectPlayer
 onready var detect_trample = $DetectTrample/Collision
+onready var animation_player = $AnimationPlayer
 
 func _on_DetectTrample_body_entered(body):
 	disable_colliders([detect_player, detect_trample])
+	animation_player.stop()
+	animation_player.play("Die")
 	body.impulse()
 
 func _on_body_entered(body):
