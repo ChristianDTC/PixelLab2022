7b61a1827080b2e180a957440bbdfe08d10fddc3
chzepech@gmail.com <chzepech@gmail.com>
2022-09-16 16:42:03 -0300

    las monedas desaparecen y emiten un sonido de captura

--- Proyecto PixelLab/Game/Objects/CoinBase.gd
+++ Proyecto PixelLab/Game/Objects/CoinBase.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
 onready var animation = $AnimatedSprite
+onready var animation_coinbase = $AnimationPlayer
+onready var collision_player = $CollisionPlayer
+
 
 func _ready():
 	animation.play()
+
+
+func _on_body_entered(body):
+	collision_player.set_deferred("disabled", true)
+	animation_coinbase.play("catch_coin")
