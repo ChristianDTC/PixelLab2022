c976e7e1c5d33efc72cc0fbb577e43257bdb1f4a
chzepech@gmail.com <chzepech@gmail.com>
2022-09-19 17:45:21 -0300

    spike modificado para que se puede usar el mismo para crear una trampa movil, falta animación, configuración y audio

--- Proyecto PixelLab/Game/Objects/Spike.gd
+++ Proyecto PixelLab/Game/Objects/Spike.gd
@@ -1,4 +1,18 @@
 extends Area2D
 
+export var is_cheat = false
+
+onready var detect_player = $DetectPlayer
+
+func _ready():
+	detect_player.enabled = true
+	if is_cheat:
+		rotation_degrees = 180
+
+func _process(delta):
+	if detect_player.is_colliding():
+		pass
+
 func _on_body_entered(body):
 	body.respawn()
+
