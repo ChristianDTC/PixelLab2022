16279b66be8319b3b9b3583fbcd38f7d0c34a690
chzepech@gmail.com <chzepech@gmail.com>
2022-09-23 03:57:30 -0300

    juego completo y funcional, cree un mini tutorial y los 4 niveles de ejemplo, falta agregar un par de niveles más y queda finalizado

--- Proyecto PixelLab/Game/Menus/MenuPrincipal.gd
+++ Proyecto PixelLab/Game/Menus/MenuPrincipal.gd
@@ -1,6 +1,9 @@
 extends Control
 
+func _ready():
+	DataPlayer.reset()
 
 func _on_ButtonStart_pressed():
 	MusicGobal.replay()
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Game/Levels/Level.1.tscn")
